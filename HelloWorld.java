94b48d304a71e3dd9b0b6fa72484becaae72af54
usreenivasulu <43570992+usreenivasulu@users.noreply.github.com>
2020-03-20 16:43:53 +0530

    updated the  file HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,9 @@
 public class HelloWorld {
+  
+  //added from develop branch
+  public void m1(){
+   System.out.println("From method m1()...");
+  }
 public static void main(String []){
   System.out.println("From Main method()...hello!");
 }
